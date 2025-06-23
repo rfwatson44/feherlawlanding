61d381b93ed6cd23adcaee1e3b89ea96b3a66823
Ryan Watson <ryanwatson@RW-MBP-16.local>
2025-06-23 11:59:55 -0700

    Added correct GTM tags for the container into layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -16,34 +17,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KY8YJTZW66"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KY8YJTZW66');
+          `}
+        </Script>
+
         {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KTFP9GN9');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
-
-        {/* Google Analytics (gtag.js), if you still need it */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-KY8YJTZW66"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-KY8YJTZW66');
-            `,
-          }}
-        />
-        {/* End Google Analytics */}
+            })(window,document,'script','dataLayer','GTM-KTFP9GN9');
+          `}
+        </Script>
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
@@ -55,7 +52,6 @@ export default function RootLayout({
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
 
         {children}
       </body>
